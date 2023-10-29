8fac64ef909ebc1a777daa509887d6f4e89d1b2c
jihanfs <148582533+jihanfs@users.noreply.github.com>
2023-10-29 10:35:44 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -2,7 +2,7 @@ const s = (p) => {
   let demo1Shader, img, fft, audio, toggleBtn
 
   p.preload = () => {
-    audio = p.loadSound('audio/uwu.mp3')
+    audio = p.loadSound('audio/Somebody Plasure.mp3')
     demo1Shader = p.loadShader('shaders/base.vert', 'shaders/d1.frag')
     img = p.loadImage('img/1.jpg')
   }
